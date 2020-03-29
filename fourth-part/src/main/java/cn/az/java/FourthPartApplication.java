ce62dcd6a900c6095d645b79a5a00e487c7e5fb1
AzusaChino <azusa146@gmail.com>
2020-03-29 19:36:22 +0800

    ft. part4 chapter1 POSIX(pthread c++)

---
+++ fourth-part/src/main/java/cn/az/java/FourthPartApplication.java
@@ -0,0 +1,16 @@
+package cn.az.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author az
+ */
+@SpringBootApplication
+public class FourthPartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourthPartApplication.class, args);
+    }
+
+}
