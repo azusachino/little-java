3d1b1df5a669c0983a717c5c8725a4374dcefde2
ycpang <ycpang@iflytek.com>
2021-03-29 19:41:40 +0800

    feat. adding security config for little-metrics

---
+++ little-boot/little-metrics/src/main/java/cn/az/boot/metrics/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package cn.az.boot.metrics.config;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author ycpang
+ * @since 2021-03-29 19:31
+ */
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // any request match endpoint check role
+        http.requestMatcher(EndpointRequest.toAnyEndpoint())
+            .authorizeRequests(r -> r.anyRequest()
+                .hasRole("ENDPOINT_ADMIN"));
+        http.httpBasic();
+        return http.build();
+    }
+}
